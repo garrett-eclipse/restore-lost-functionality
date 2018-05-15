b262e9683fb372ca1f251bc37d362ed9c87167b6
David Artiss <david.artiss@automattic.com>
2018-05-15 11:23:19 +0100

    Version 3.1.1

--- includes/options-screen.php
+++ includes/options-screen.php
@@ -4,8 +4,8 @@
 *
 * Screen for allowing users to specify which functionality to restore
 *
-* @package	Restore-lost-Functionality
-* @since	1.0
+* @package  Restore-lost-Functionality
+* @since    1.0
 */
 ?>
 <div class="wrap">
@@ -17,7 +17,7 @@ $screen_options = restore_wp_screen_options();
 
 // If options have been updated on screen, update the database
 
-if ( ( !empty( $_POST ) ) && ( check_admin_referer( 'restore-wp-options', 'restore_wp_options_nonce' ) ) ) { // Input var okay.
+if ( ( ! empty( $_POST ) ) && ( check_admin_referer( 'restore-wp-options', 'restore_wp_options_nonce' ) ) ) { // Input var okay.
 
 	// Loop through the options array. If a valid option that requires storing, santitize the
 	// field appropriately
@@ -27,12 +27,12 @@ if ( ( !empty( $_POST ) ) && ( check_admin_referer( 'restore-wp-options', 'resto
 
 		$option = $screen_options[ $loop ];
 
-		if ( !$option[ 0 ] && $option[ 1 ] ) {
+		if ( ! $option[0] && $option[1] ) {
 
-			if ( isset( $_POST[ 'restore_wp_' . $option[ 2 ] ] ) ) { // Input var okay.
-				$options[ $option[ 2 ] ] = sanitize_text_field( $_POST[ 'restore_wp_' . $option[ 2 ] ] ); // Input var okay.
+			if ( isset( $_POST[ 'restore_wp_' . $option[2] ] ) ) { // Input var okay.
+				$options[ $option[2] ] = sanitize_text_field( $_POST[ 'restore_wp_' . $option[2] ] ); // Input var okay.
 			} else {
-				$options[ $option[ 2 ] ] = '';
+				$options[ $option[2] ] = '';
 			}
 		}
 
@@ -49,10 +49,11 @@ if ( ( !empty( $_POST ) ) && ( check_admin_referer( 'restore-wp-options', 'resto
 // Get options
 
 $options = get_option( 'restore_wp_options' );
-if ( !is_array( $options ) ) { $options = array(); }
+if ( ! is_array( $options ) ) {
+	$options = array(); }
 ?>
 
-<form method="post" action="<?php echo esc_attr( get_bloginfo( 'wpurl' ) ) . '/wp-admin/options-general.php?page=restore-wp-options' ?>">
+<form method="post" action="<?php echo esc_attr( get_bloginfo( 'wpurl' ) ) . '/wp-admin/options-general.php?page=restore-wp-options'; ?>">
 
 <table class="form-table">
 
@@ -65,12 +66,12 @@ while ( $loop < count( $screen_options ) ) {
 
 	$option = $screen_options[ $loop ];
 
-	if ( ( ( float ) $wp_version >= $option[ 6 ] && 'jetpack' !== $option[ 6 ] ) or ( 'jetpack' === $option[ 6 ] && is_plugin_active( 'jetpack/jetpack.php' ) ) ) {
+	if ( ( (float) $wp_version >= $option[6] && 'jetpack' !== $option[6] ) or ( 'jetpack' === $option[6] && is_plugin_active( 'jetpack/jetpack.php' ) ) ) {
 
 		// If a plugin, check it's installation status and prepare an appropriate message
 
-		if ( $option[ 0 ] ) {
-			$status = restore_wp_check_plugin( $option[ 4 ] );
+		if ( $option[0] ) {
+			$status = restore_wp_check_plugin( $option[4] );
 			if ( 0 === $status ) {
 				$text = __( 'Install the plugin', 'restore-lost-functionality' );
 			} else {
@@ -84,20 +85,21 @@ while ( $loop < count( $screen_options ) ) {
 
 		// Output the initial part of the form
 
-		echo '<tr>'  . "\n" . '<th scope="row">' . esc_attr( ucwords( $option[ 3 ] ) ) . ' </th>' . "\n";
-		echo '<td><fieldset><legend class="screen-reader-text"><span>' . esc_attr( ucwords( $option[ 3 ] ) ) . ' </span></legend>' . "\n";
+		echo '<tr>' . "\n" . '<th scope="row">' . esc_attr( ucwords( $option[3] ) ) . ' </th>' . "\n";
+		echo '<td><fieldset><legend class="screen-reader-text"><span>' . esc_attr( ucwords( $option[3] ) ) . ' </span></legend>' . "\n";
 
 		// Add the checkbox, read-only if a plugin and whether a relevant current status
 
-		if ( $option[ 1 ] ) {
+		if ( $option[1] ) {
 
-			echo "\t" . '<label for="restore_wp_' . esc_attr( $option[ 2 ] ) . '"><input ';
-			if ( $option[ 0 ] ) { echo 'disabled="disabled" '; }
-			echo 'type="checkbox" name="restore_wp_' . esc_attr( $option[ 2 ] ) . '" id="restore_wp_' . esc_attr( $option[ 2 ] ) . '" value="1" ';
-			if ( $option[ 0 ] ) {
+			echo "\t" . '<label for="restore_wp_' . esc_attr( $option[2] ) . '"><input ';
+			if ( $option[0] ) {
+				echo 'disabled="disabled" '; }
+			echo 'type="checkbox" name="restore_wp_' . esc_attr( $option[2] ) . '" id="restore_wp_' . esc_attr( $option[2] ) . '" value="1" ';
+			if ( $option[0] ) {
 				checked( $status, 2 );
 			} else {
-				checked( $options[ $option[ 2 ] ], 1 );
+				checked( $options[ $option[2] ], 1 );
 			}
 			echo '/>';
 
@@ -106,8 +108,8 @@ while ( $loop < count( $screen_options ) ) {
 		// Depending on whether this is a plugin or not, either output a short description
 		// or a link to the plugin
 
-		if ( $option[ 0 ] ) {
-			$url = admin_url( 'plugin-install.php?s=' . $option[ 4 ] . '&tab=search&type=term' );
+		if ( $option[0] ) {
+			$url = admin_url( 'plugin-install.php?s=' . $option[4] . '&tab=search&type=term' );
 			echo '<a href="' . esc_url( $url ) . '" aria-label="' . esc_attr__( 'Display information about the plugin', 'restore-lost-functionality' ) . '">' . esc_attr__( $text ) . '</a>';
 		} else {
 			echo $option[ 4 ]; // @codingStandardsIgnoreLine -- Source array is escaped. Not doing it again here, otherwise it strips valid HTML from the content.
--- uninstall.php
+++ uninstall.php
@@ -4,13 +4,14 @@
 *
 * Uninstall the plugin by removing any options from the database
 *
-* @package	Restore-WordPress-Functionality
-* @since	1.0
+* @package  Restore-WordPress-Functionality
+* @since    1.0
 */
 
 // If the uninstall was not called by WordPress, exit
 
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit(); }
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit(); }
 
 // Now remove any data created by the plugin
 
